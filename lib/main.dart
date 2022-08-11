ec4bd09fe1b8f78f9b31c94a6dba0dd5fd122a45
carlstep <carlstep333@me.com>
2022-08-11 13:32:12 +0700

    final code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_routes_themes/routes/routes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
+      // theme: ThemeData(
+      //   primaryColor: Colors.amber,
+      //   scaffoldBackgroundColor: Colors.blue[100],
+      // ),
       initialRoute: RouteManager.homePage,
       onGenerateRoute: RouteManager.generateRoute,
     );
